ad087e017ef0709348f7196b59524ab937618370
Matthew <46129554+MatthewSummons@users.noreply.github.com>
2024-01-24 18:46:13 +0800

    Dogshit Server App

--- Labs/Lab 1/lab1_materials/server.py
+++ Labs/Lab 1/lab1_materials/server.py
@@ -14,6 +14,34 @@ class ServerThread(threading.Thread):
         connectionSocket, addr = self.client
 
         # Step 4: read the client's guesses and send responses until the client guesses correctly
+        guess = ''
+        while guess != self.secret:    
+            # Obtain the client's complete guess
+            response = ''
+            while not response.endswith('\n'):
+                response_buffer = connectionSocket.recv(1024)
+
+                if len(response_buffer) == 0:
+                    connectionSocket.close()
+
+                response += response_buffer.decode()
+            print("Received guess: ", response, end="")
+            try:
+                guess = int(response)
+            except ValueError:
+                connectionSocket.close()
+            
+            msg = None
+            if guess < self.secret:
+                msg = "Too low"
+            elif guess > self.secret:
+                msg = "Too high"
+            else:
+                msg = "You win!"
+            
+            connectionSocket.send((msg + "\n").encode())
+        connectionSocket.close()
+
 
 
 
@@ -42,14 +70,18 @@ class ServerMain:
     def server_run(self):
         # Step 3: set up `serverSocket` that listens for TCP connections on port 12000
         # Hint: refer to socketprog_examples/TCPSocket-5
-
-
-
+        serverPort = 12000
+        serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)    # Use TCP
+        serverSocket.bind( ("", serverPort) )
+        serverSocket.listen(5)      # Backlog of 5
 
         print("Game started! The secret is", self.secret)
 
         while True:
             # Step 3: accept a connection and launch a thread to handle the connection. Pass appropriate arguments to the thread constructor
+            client = serverSocket.accept()
+            t = ServerThread(client, self.secret)
+            t.start()
 
 
 
